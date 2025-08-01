0530bf0b27387830c9031e6e6c3688f1d29d4e6b
Ashutosh Swamy <ashutoshswamy397@gmail.com>
2025-08-01 18:55:45 +0000

    use brain related icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, BrainCircuit } from 'lucide-react';
 
 const Logo = () => (
-  <svg
-    width="32"
-    height="32"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="text-primary"
-  >
-    <path
-      d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
-      fill="currentColor"
-    />
-    <path
-      d="M12 7c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3zm0 4c-.552 0-1-.448-1-1s.448-1 1-1 1 .448 1 1-.448 1-1 1z"
-      fill="currentColor"
-    />
-    <path
-      d="M15.5 14.5a.997.997 0 0 1-.707-.293l-2.793-2.793-2.793 2.793a.999.999 0 1 1-1.414-1.414l3.5-3.5a.999.999 0 0 1 1.414 0l3.5 3.5a.999.999 0 0 1-.707 1.707z"
-      fill="currentColor"
-      transform="rotate(45 12 12.5)"
-    />
-  </svg>
+  <BrainCircuit className="h-8 w-8 text-primary" />
 );
 
 
